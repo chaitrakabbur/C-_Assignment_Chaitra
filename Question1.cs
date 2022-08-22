5c1f5ea4d367bd506d4c1798b567399f80f96b6c
chaitrakabbur <68099758+chaitrakabbur@users.noreply.github.com>
2022-08-22 23:00:28 +0530

    Add files via upload

---
+++ Question1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Question1
+    {
+        public static void Main()
+        {
+            int a;
+            int b;
+            int c;
+            int d;
+            a =int.Parse( Console.ReadLine());
+            b =int.Parse( Console.ReadLine());
+            c =int.Parse(Console.ReadLine());
+
+            d = a * b * c;
+                Console.WriteLine(d);
+        }
+    }
+}
+
