951c99d8a08fe4b827f0a18d3f1ce2e2a4993ddb
chaitrakabbur <68099758+chaitrakabbur@users.noreply.github.com>
2022-09-21 21:58:07 +0530

    Add files via upload
    Minimum Maximum

---
+++ MinMaxNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class MinMaxNum
+    {
+        public static void Main()
+        {
+            int n;
+
+            Console.WriteLine("Enter the array number: ");
+            n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            Console.WriteLine("Enter the array values");
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Maximum number is {numbers.Max()} ");
+            Console.WriteLine($"Minimum number is {numbers.Min()} ");
+        }
+    }
+}
